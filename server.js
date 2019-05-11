6aa974d8318ad7588d62d3a3a4ef0fb1045d3b09
Nitin Pamnani <nitin_pamnani@trekbikes.com>
2019-05-11 23:50:00 +0530

    changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import http from "http";
 import app from "./app";
 
-const port = process.env.port || 3029;
+const port = process.env.PORT || 3029;
 
 const server = http.createServer(app);
 server.listen(port, function(){
-    console.log("app running on port"+process.env.port);
+    console.log("app running on port"+process.env.PORT);
 });
